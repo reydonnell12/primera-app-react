1ad58f26e36c18fafd094b5cfa9b061280b2f236
reydonnell <programacion.expert.rey@gmail.com>
2025-05-13 19:25:54 -0500

    segundo commit para probar

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
 import rey_atanagildo from "./img/rey_atanagildo.png";
 import rey_leogivildo from "./img/rey_leogivildo.png";
 import rey_sisebuto from "./img/rey_sisebuto.png";
+import rey_ataulfo from "./img/rey_ataulfo.png";
 
 import "./App.css";
 export const App = () => {
-  let names = ["rey_sisebuto", "rey_leogivildo", "rey_atanagildo"];
+  let names = [
+    "rey_sisebuto",
+    "rey_leogivildo",
+    "rey_atanagildo",
+    "rey_ataulfo",
+  ];
 
   return (
     <>
@@ -21,6 +27,11 @@ export const App = () => {
       <div className="im">
         <img src={rey_atanagildo} alt={names[0]} className="im" />
         <p>{names[2]}</p>
+      </div>
+
+      <div className="im">
+        <img src={rey_ataulfo} alt={names[3]} className="im" />
+        <p>{names[3]}</p>
       </div>
     </>
   );
